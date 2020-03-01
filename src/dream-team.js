950d4aee04ec3679b7f3d8e838f8dcdbe35533ea
GanulAnton <47523214+GanulAnton@users.noreply.github.com>
2020-03-01 23:10:00 +0300

    feat: add function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  var DtArr = [];
+  var letter;
+  var Dt;
+  if (!Array.isArray(arr)) {
+    return false;
+  }
+  arr.forEach(function(el) {
+    if (typeof el == "string") {
+      el = el.replace(/\s+/g, "");
+      letter = el[0];
+      DtArr.push(letter.toUpperCase());
+    }
+  });
+  Dt = DtArr.sort().join("");
+  return Dt;
 };
