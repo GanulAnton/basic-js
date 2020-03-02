a8c7efedf6975ab14cfc8400b11ea0506f7136f6
GanulAnton <47523214+GanulAnton@users.noreply.github.com>
2020-03-02 14:23:24 +0300

    feat: update function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,88 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+  constructor(bool = true) {
+    this.bool = bool;
+  }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error("Error");
     }
+    let resultArr = [];
+    let t = 0;
+    for (let i = 0; i < message.length; i++) {
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+      if (message[i].match(/[A-Z]/)) {
+        if (message.indexOf(message[i]) < 0) {
+          resultArr.push(
+            String.fromCodePoint(
+              ((message[i].charCodeAt(0) -
+                "A".charCodeAt(0) +
+                (26 -
+                  (key[t % key.length].charCodeAt(0) - "A".charCodeAt(0)))) %
+                26) +
+                "A".charCodeAt(0)
+            )
+          );
+        } else {
+          resultArr.push(
+            String.fromCodePoint(
+              ((message[i].charCodeAt(0) -
+                "A".charCodeAt(0) +
+                (key[t % key.length].charCodeAt(0) - "A".charCodeAt(0))) %
+                26) +
+                "A".charCodeAt(0)
+            )
+          );
+        }
+        t++;
+      } else {
+        resultArr.push(message[i]);
+      }
+    }
+    return this.bool === false
+      ? resultArr.reverse().join("")
+      : resultArr.join("");
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error("Error");
+    }
+    let result = [];
+    let t = 0;
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      encryptedMessage = encryptedMessage.toUpperCase();
+      key = key.toUpperCase();
+      if (encryptedMessage[i].match(/[A-Z]/)) {
+        if (encryptedMessage.indexOf(encryptedMessage[i]) < 0) {
+          result.push(
+            String.fromCodePoint(
+              ((encryptedMessage[i].charCodeAt(0) -
+                "A".charCodeAt(0) +
+                (key[t % key.length].charCodeAt(0) - "A".charCodeAt(0))) %
+                26) +
+                "A".charCodeAt(0)
+            )
+          );
+        } else {
+          result.push(
+            String.fromCodePoint(
+              ((encryptedMessage[i].charCodeAt(0) -
+                "A".charCodeAt(0) +
+                (26 -
+                  (key[t % key.length].charCodeAt(0) - "A".charCodeAt(0)))) %
+                26) +
+                "A".charCodeAt(0)
+            )
+          );
+        }
+        t++;
+      } else {
+        result.push(encryptedMessage[i]);
+      }
+    }
+    return this.bool === false ? result.reverse().join("") : result.join("");
+  }
 }
-
 module.exports = VigenereCipheringMachine;
